1cf2e7812bf7a7021d4487a1a36b920a4b6a246f
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-16 03:45:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def square_array(array)
   new_array = []
   index = 0
   while index < array.length do
-    new_array << array[index]*2
+    new_array << array[index]*array[index]
     index += 1
  end
  new_array
