1bc834afbc51de982a4f38bb3e9c55ebc57bf9e3
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-16 03:43:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,13 @@
 def square_array(array)
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
+  new_array = []
+  index = 0
+  while index < array.length do
+    new_array << array[index]*2
+    index += 1
+ end
+ new_array
 end
 
 def summon_captain_planet(planeteer_calls)
